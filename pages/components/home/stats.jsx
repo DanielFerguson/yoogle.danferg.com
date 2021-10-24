2de5d0fbd80910de97ab50e97b234d49742217ab
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2021-10-24 15:56:57 +1100

    fixed unescaped entities

--- pages/components/home/stats.jsx
+++ pages/components/home/stats.jsx
@@ -3,9 +3,9 @@ export default function Stats() {
         <div className="bg-indigo-800">
             <div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8 lg:py-20">
                 <div className="max-w-4xl mx-auto text-center">
-                    <h2 className="text-3xl font-extrabold text-white sm:text-4xl">We've got some lovely nice stats for you...</h2>
+                    <h2 className="text-3xl font-extrabold text-white sm:text-4xl">We&apos;ve got some lovely nice stats for you...</h2>
                     <p className="mt-3 text-xl text-indigo-200 sm:mt-4">
-                        I'd love to say that these stats were meaningful, but honestly don't read into them too much.
+                        I&apos;d love to say that these stats were meaningful, but honestly don&apos;t read into them too much.
                     </p>
                 </div>
                 <dl className="mt-10 text-center sm:max-w-3xl sm:mx-auto sm:grid sm:grid-cols-3 sm:gap-8">
